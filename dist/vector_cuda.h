99399aa0749791b746da6c7136fad54b972d5721
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-06-24 20:40:50 +0530

    vector_cuda.h added

--- dist/vector_cuda.h
+++ dist/vector_cuda.h
@@ -5,6 +5,7 @@
 #include <cuda_runtime_api.h>
 #include <iostream>
 #include <cuda_runtime.h>
+#include <iterator>
 
 
 class Vector{
@@ -12,7 +13,7 @@ class Vector{
         double h_x,h_y,h_z;
         double *d_x,*d_y,*d_z;
 
-        void cudaError(cudaError_t err,const char *msg){
+        void cudaError(cudaError_t err,const char *msg) const{
             if(err != cudaSuccess){
                 std::cerr << "CUDA Error: " << msg << " - " << cudaGetErrorString(err) << std::endl;
                 exit(EXIT_FAILURE);
@@ -25,6 +26,7 @@ class Vector{
 
         void cpyToHost();
         void print() const;
+        std::string str() const;
 
         Vector add(const Vector &vec);
         Vector add(const double &scalar);
@@ -47,6 +49,21 @@ class Vector{
         double dot(const Vector &vec);
         double norm();
         double unit();
+        Vector proj(const Vector &onto);
+
+        bool operator==(const Vector &vec) const;
+        bool operator!=(const Vector &vec) const;
+        bool operator>=(const Vector &vec) const;
+        bool operator>(const Vector &vec) const;
+        bool operator<=(const Vector &vec) const;
+        bool operator<(const Vector &vec) const;
+
+        bool eq(const Vector &vec) const;
+        bool ne(const Vector &vec) const;
+        bool gt(const Vector &vec) const;
+        bool ge(const Vector &vec) const;
+        bool lt(const Vector &vec) const;
+        bool le(const Vector &vec) const;
 
         static Vector zeros();
         static Vector ones();
@@ -76,6 +93,14 @@ __global__ void dotKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2
 __global__ void crossKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, double *r_d_x, double *r_d_y, double *r_d_z);
 __global__ void normKernel(double *d_x, double *d_y, double *d_z, double *result);
 __global__ void unitKernel(double *d_x, double *d_y, double *d_z, double *r_d_x, double *r_d_y, double *r_d_z);
+__global__ void projKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, double *r_d_x, double *r_d_y, double *r_d_z);
+
+__global__ void eqKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, bool *isEq);
+__global__ void neKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, bool *isNe);
+__global__ void gtKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, bool *isGt);
+__global__ void geKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, bool *isGe);
+__global__ void ltKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, bool *isLt);
+__global__ void leKernel(double *d_x1, double *d_y1, double *d_z1, double *d_x2, double *d_y2, double *d_z2, bool *isLe);
 
 #endif
 
